b319b1ad91023eaeab16936b2e8a99e123a23568
Abhaymn <abhaykrishnatsy007>
2022-10-19 11:55:23 +0530

    changess

---
+++ dom.js
@@ -0,0 +1,11 @@
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color='green';
+
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+
+
+var odd =document.querySelectorAll('li:nth-child(odd)');
+for (var i=0;i<odd.length ; i++){
+    odd[i].style.backgroundColor='green';
+}
